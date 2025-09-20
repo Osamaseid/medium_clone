51fbd48605ab6688427e870a879abfa9d069e55f
osama seid <osaseid1@gmail.com>
2025-09-20 22:53:23 +0300

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\category;
 use App\Models\Post;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use Illuminate\Database\Seeder;
@@ -16,7 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-      
+      User::factory()->create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+      ]);
 
 
       $categories = [
